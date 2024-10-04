e776f0106f06670eab6d705a51fc2d0c4ac6d89d
JaydeepRokade <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-04 19:20:23 +0530

    Update leet10.java

--- leet10.java
+++ leet10.java
@@ -10,8 +10,8 @@ class Solution {
         long chemistrySum = 0;
 
         // Step 2: Pair players using two pointers
-        for (int i = 0; i < n / 2; i++) {
-            // Check if the sum of current pair matches the required totalSkill
+        for (int i = 0; i < n / 2; i++) 
+            {
             if (skill[i] + skill[n - i - 1] != totalSkill) {
                 return -1; // Invalid configuration, return -1
             }
