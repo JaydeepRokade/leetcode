4166b21aec3a8b8270639973fb8ea27f7d9f2678
JaydeepRokade <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-05 12:54:25 +0530

    Update leet11.java

--- leet11.java
+++ leet11.java
@@ -2,12 +2,10 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
     int n1 = s1.length();
     int n2 = s2.length();
-   
     if(n1>n2)
     return false;
     if(n1==0)
     return true;
-
     int a[] = new int[26];
     int b[] = new int[26];
 
