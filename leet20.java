0a158cc1aeeaa39c77362973a82afe1d71c39960
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-13 18:50:14 +0530

    Update leet20.java

--- leet20.java
+++ leet20.java
@@ -4,7 +4,7 @@ class Solution {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         int curMax = Integer.MIN_VALUE;
 
-        // Initialize the heap with the first element of each list
+
         for (int i = 0; i < nums.size(); i++) {
             minHeap.offer(new int[]{nums.get(i).get(0), i, 0});
             curMax = Math.max(curMax, nums.get(i).get(0));
