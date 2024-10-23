5b9fb30b946827cbdbe78399368a2be4827f0eed
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-23 13:56:29 +0530

    Update leet29.java

--- leet29.java
+++ leet29.java
@@ -4,17 +4,14 @@ class Solution {
         root.val = 0;
         return root;
     }
-
     private void dfs(TreeNode[] arr) {
         if (arr.length == 0) return;
-
         int sum = 0;
         for (TreeNode node : arr) {
             if (node == null) continue;
             if (node.left != null) sum += node.left.val;
             if (node.right != null) sum += node.right.val;
         }
-
         TreeNode[] childArr = new TreeNode[arr.length * 2];
         int index = 0;
 
