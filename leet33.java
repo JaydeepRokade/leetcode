a658345dace35871d974b1f1a2d0c3e1b60e803b
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-26 17:17:04 +0530

    Update leet33.java

--- leet33.java
+++ leet33.java
@@ -1,8 +1,12 @@
 class Solution {
     int depth[];
+    
     int levelArr[];
+    
     int max1[];
-    int max2[];  
+    
+    int max2[]; 
+    
     public int height(TreeNode root, int level)
     {
      if(root==null)
