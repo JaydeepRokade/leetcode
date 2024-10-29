98e246925f78bbe48bc7d2194421180449bcf172
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-29 18:54:35 +0530

    Update leet36.cpp

--- leet36.cpp
+++ leet36.cpp
@@ -4,8 +4,6 @@ public:
         int m = grid.size();  
         int n = grid[0].size(); 
         int res = 0;
-
-        
         vector<int> dp(m);
         
         for (int j = 1; j < n; ++j) {
@@ -13,36 +11,26 @@ public:
             bool found = false;
             
             for (int i = 0; i < m; ++i) {
-                // cur will store the maximum moves to reach current cell
                 int cur = -1;
-                // Store dp[i] for next iteration's leftTop
                 int nxtLeftTop = dp[i];
                 
-                // Check move from top-left cell (if valid)
                 if (i - 1 >= 0 && leftTop != -1 && grid[i][j] > grid[i - 1][j - 1]) {
                     cur = max(cur, leftTop + 1);
                 }
-                
-                // Check move from direct left cell (if valid)
+
                 if (dp[i] != -1 && grid[i][j] > grid[i][j - 1]) {
                     cur = max(cur, dp[i] + 1);
                 }
-                
-                // Check move from bottom-left cell (if valid)
                 if (i + 1 < m && dp[i + 1] != -1 && grid[i][j] > grid[i + 1][j - 1]) {
                     cur = max(cur, dp[i + 1] + 1);
                 }
                 
-                // Update dp array for current cell
                 dp[i] = cur;
-                // Update found flag if we can reach current cell
                 found = found || (dp[i] != -1);
-                // Update leftTop for next row's iteration
                 leftTop = nxtLeftTop;
             }
             
             if (!found) break;
-            // Update result to current column if we can reach it
             res = j;
         }
         
