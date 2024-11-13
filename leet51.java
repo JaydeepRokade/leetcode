12f5916a9068df8686ca97ae3f9f5ac52b466bf2
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-13 08:58:24 +0530

    Update leet51.java

--- leet51.java
+++ leet51.java
@@ -3,7 +3,6 @@ class Solution {
     Arrays.sort(nums);
     return countLess(nums, upper) - countLess(nums, lower - 1);
   }
-
   private long countLess(int[] nums, int sum) {
     long res = 0;
     for (int i = 0, j = nums.length - 1; i < j; ++i) {
