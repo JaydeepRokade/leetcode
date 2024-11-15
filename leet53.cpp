73f0ecb4ec4c806bc62edff3202c3e636baa1955
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-15 11:14:39 +0530

    Update leet53.cpp

--- leet53.cpp
+++ leet53.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
         int n = arr.size();
-        
         int left = 0;
         while (left + 1 < n && arr[left] <= arr[left + 1]) {
             left++;
         }
-        
         if (left == n - 1) return 0;
        
         int right = n - 1;
