1a1bd359ac661fdd084164255e11bd81fbf5be5a
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-15 11:14:53 +0530

    Update leet53.cpp

--- leet53.cpp
+++ leet53.cpp
@@ -7,12 +7,10 @@ public:
             left++;
         }
         if (left == n - 1) return 0;
-       
         int right = n - 1;
         while (right > 0 && arr[right - 1] <= arr[right]) {
             right--;
-        }
-        
+        }  
         int result = min(n - left - 1, right);
        
         int i = 0, j = right;
