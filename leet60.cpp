51ad20ba752fd98b99bc1e0e93b694b9b7d970d7
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-23 21:13:35 +0530

    Update leet60.cpp

--- leet60.cpp
+++ leet60.cpp
@@ -15,7 +15,6 @@ public:
                 }
             }
         }
-        
         vector<vector<char>> rotatedBox(n, vector<char>(m));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
