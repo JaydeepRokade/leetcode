3fc6b17323f71fefb79f4f562e6c448e8e847095
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-12-07 19:09:53 +0530

    Update leet94.cpp

--- leet94.cpp
+++ leet94.cpp
@@ -7,7 +7,7 @@ public:
             if (ops > maxOperations) return false;
         }
         return true;
-    
+    }
     int minimumSize(vector<int>& nums, int maxOperations) {
         int left = 1, right = *max_element(nums.begin(), nums.end());
         int res = right;
