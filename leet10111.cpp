e5137fce72facb9d07d0942050d144eb9476f4a4
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-01-19 22:46:05 +0530

    Update leet10111.cpp

--- leet10111.cpp
+++ leet10111.cpp
@@ -12,14 +12,11 @@ class Solution {
         if(k == 0){
             return head;
         }
-        
         temp.next = head;
-        
         Node newTail = head;
         for(int i = 1; i < k; i++){
             newTail = newTail.next;
         }
-        
         Node newHead = newTail.next;
         newTail.next = null;
         return newHead;
