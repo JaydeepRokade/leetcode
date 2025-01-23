aead3192259b9611ea2b32288645b545868b6e0d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-01-23 17:25:59 +0530

    Update leet201.cpp

--- leet201.cpp
+++ leet201.cpp
@@ -6,11 +6,8 @@ public:
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if (grid[i][j]){
-                    
                     cnt++;
-                    
                     row[i]++;
-                    
                     col[j]++;
                 }
             }
