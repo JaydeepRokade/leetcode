68183f5d0154c9935489dbf48423d42428a0b6d9
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-01-28 23:45:30 +0530

    Update leet203.cpp

--- leet203.cpp
+++ leet203.cpp
@@ -6,7 +6,6 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int maxFish = 0;       
-
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 0) continue;    
