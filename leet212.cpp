c1bcb87029717b587e21930f611cc402de23f236
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-02-21 23:51:57 +0530

    Update leet212.cpp

--- leet212.cpp
+++ leet212.cpp
@@ -5,11 +5,7 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+
 bitset<1048576> hasX = 0; // 2097152=2**21
 class FindElements {
 public:
