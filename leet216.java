4d4da9b7ea95e0bd0a69c741e05ac1c63bedd6a2
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-02-26 23:59:45 +0530

    Update leet216.java

--- leet216.java
+++ leet216.java
@@ -4,6 +4,7 @@ class Solution {
         for (int num : nums) {
             sum += num;
             if (sum > maxSum) maxSum = sum;
+            
             if (sum < minSum) minSum = sum;
         }
         return Math.abs(maxSum - minSum);
