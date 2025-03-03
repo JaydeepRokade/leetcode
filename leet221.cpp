be4bb1cf287942bc478a8cf2ecaf274bca035035
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-03 23:44:24 +0530

    Update leet221.cpp

--- leet221.cpp
+++ leet221.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> result(nums.size(), 0);
         int left = 0, right = nums.size() - 1;
-        
         for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
             if (nums[i] < pivot) {
                 result[left] = nums[i];
