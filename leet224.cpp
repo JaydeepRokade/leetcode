8ae8b3f5c7955c808eab6b69fa2e2edf2af42fa8
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-09 23:50:05 +0530

    Update leet224.cpp

--- leet224.cpp
+++ leet224.cpp
@@ -4,7 +4,7 @@ public:
         colors.insert(colors.end(), colors.begin(), colors.begin() + (k - 1));
         int count = 0;
         int left = 0;
-        // jaydepassodlvllvkxksnclscnxnxncahasajseeuidoclacjc
+        // jaydepassodlvllvkxksnclscnxnxncahasajseeui
         for (int right = 0; right < colors.size(); ++right) {
             if (right > 0 && colors[right] == colors[right - 1]) {
                 left = right;  
