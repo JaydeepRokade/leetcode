793750810e773fb2a336503545a2dd4263f52b7d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-14 23:49:40 +0530

    Update leet230.cpp

--- leet230.cpp
+++ leet230.cpp
@@ -3,7 +3,7 @@ public:
     int maximumCandies(vector<int>& candies, long long k) {
         long long left = 1, right = *max_element(candies.begin(), candies.end());
         int result = 0;
-        //dwkdjw djdl dcldcl llkdlkvlk lkdvov;  lkvdsvs dkdv dd
+        //dwkdjw djdl dcldcl llkdlkvlk lkdvov;  lkvdsv
         while (left <= right) {
             long long mid = left + (right - left) / 2;
             long long children_count = 0;
