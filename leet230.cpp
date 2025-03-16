2c4942e2589c1c05b092758dfdfa3f3603f00bee
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-16 22:24:45 +0530

    Update leet230.cpp

--- leet230.cpp
+++ leet230.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int maximumCandies(vector<int>& candies, long long k) {
-        long long left = 1, right = *max_element(candies.begin(), candies.end());
-        int result = 0;
-        //dwkdj
-        while (left <= right) {
-            long long mid = left + (right - left) / 2;
-            long long children_count = 0;
-
-            for (int pile : candies) {
-                children_count += pile / mid;
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long left = 1, right = (long long)*min_element(ranks.begin(), ranks.end()) * cars * cars;
+        
+        auto can_repair_all = [&](long long time) {
+            long long total_cars_repaired = 0;
+            for (int rank : ranks) {
+                total_cars_repaired += sqrt(time / rank);
+                if (total_cars_repaired >= cars) return true;
             }
-
-            if (children_count >= k) {
-                result = mid;
-                left = mid + 1;
+            return false;
+        };
+        
+        while (left < right) {
+            long long mid = (left + right) / 2;
+            if (can_repair_all(mid)) {
+                right = mid;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
-
-        return result;
+        
+        return left;
     }
 };
