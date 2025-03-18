f49ba33a380628af0fb528d801ab0cc897b9ce44
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-19 00:00:25 +0530

    Update leet240.cpp

--- leet240.cpp
+++ leet240.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 
 using namespace std;
-//akms;xa;l mxlam ;alxmalm; ;lma ;ak ;
+//akms;xa;l mxlam ;alxm
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
