e95206eac42ed47aa931b0ccc239f2a3194ddffb
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-19 23:59:28 +0530

    Update leet52.cpp

--- leet52.cpp
+++ leet52.cpp
@@ -6,7 +6,7 @@ public:
     int minOperations(vector<int>& nums) {
         int count = 0;
         int n = nums.size();
-        
+        //csdsd csff  sffsf xvvvddv kuis cssdv xvvd
         for (int i = 0; i < n - 2; i++) {
             if (nums[i] == 0) {
                 nums[i] ^= 1;
