83fb32425001a61fb3ee1a76e0108d415cb1c77f
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-23 00:00:14 +0530

    Update leet238.cpp

--- leet238.cpp
+++ leet238.cpp
@@ -10,7 +10,7 @@ public:
             }
             return parent[x];
         };
-        //vldfsk sfgk sgloso fgkksal dfgpjsjj pfdgk ldglhl ;bldn
+        //vldfsk sfgk sgloso fgkksal dfgpjsjj pfdgk ldg
         auto unionSets = [&](int x, int y) {
             int rootX = find(x);
             int rootY = find(y);
