0022e4a716c384f4afc56c96203c18c1575766b0
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-25 01:00:25 +0530

    Update leet237.cpp

--- leet237.cpp
+++ leet237.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
-        //sxkskk jsnns
+        //sxks
         vector<vector<int>> mergedMeetings;
         for (const auto& meeting : meetings) {
             if (mergedMeetings.empty() || meeting[0] > mergedMeetings.back()[1]) {
