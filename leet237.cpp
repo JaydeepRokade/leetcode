9eb0df0e2ba987d8dff57d55d243cec9c85c49af
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-25 00:57:31 +0530

    Update leet237.cpp

--- leet237.cpp
+++ leet237.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
-        //sxkskk jsnnslkk ks/kdlklldca.dv ndkjnnsvkj kjcsj,ncn//lnvljnclj nljcnwlnc lndljn'l ndvls'llj ljcsslcc jncvljnflj ldjvnen jdvljdn ldvl 'ln 'dnv'dl lnc lfjbljdal l n'lvvldldl vdv''dv  vavvvv'
+        //sxkskk jsnnslkk ks/kdlklldca.dv ndkjnnsvkj kjcsj,ncn//lnvljnclj nljcnwlnc lndljn'l ndvls'llj ljcsslcc jncvljnflj ldjvnen jdvljdn ldvl 'ln 'dnv'dl lnc lfjbljdal l n'lvvldldl vdv''dv  vav
         vector<vector<int>> mergedMeetings;
         for (const auto& meeting : meetings) {
             if (mergedMeetings.empty() || meeting[0] > mergedMeetings.back()[1]) {
