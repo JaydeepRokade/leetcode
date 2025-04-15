ca9cfbbb7531fdeb9cafb0baa4a293eed298f5c1
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-04-16 00:01:37 +0530

    Update leet10111.cpp

--- leet10111.cpp
+++ leet10111.cpp
@@ -17,7 +17,7 @@ class Solution {
             newTail = newTail.next;
         }
 
-// DACA DVD ffvr DV e dfv axe mi LTC EDC for w
+// DACA DVD ffvr DV e dfv axe mi LTC ED
 
         Node newHead = newTail.next;
         newTail.next = null;
