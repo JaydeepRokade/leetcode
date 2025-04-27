f4c9238f129e3d9c309f5cd96949200091bd2948
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-04-27 23:59:48 +0530

    Update leet52.cpp

--- leet52.cpp
+++ leet52.cpp
@@ -1,21 +1,9 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    int minOperations(vector<int>& nums) {
-        int count = 0;
-        int n = nums.size();
-       
-        for (int i = 0; i < n - 2; i++) {
-            if (nums[i] == 0) {
-                nums[i] ^= 1;
-                nums[i + 1] ^= 1;
-                nums[i + 2] ^= 1;
+    int countSubarrays(vector<int>& nums, int count = 0) {
+        for (int i = 0 ; i + 2 < nums.size(); i++)
+            if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) 
                 count++;
-            }
-        }
-        
-        return (nums[n - 2] == 1 && nums[n - 1] == 1) ? count : -1;
+        return count;
     }
 };
