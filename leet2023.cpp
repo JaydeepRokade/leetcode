a80894d6cd1d66d4609ea3db5e7d075f0ba9068d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-04-28 23:47:05 +0530

    Update leet2023.cpp

--- leet2023.cpp
+++ leet2023.cpp
@@ -5,6 +5,7 @@ public:
         ll count = 0, sum = 0, left = 0;
         for (int right = 0; right < nums.size(); right++) {
             sum += nums[right];
+            
             while (sum * (right - left + 1) >= k) {
                 sum -= nums[left++];
                 
