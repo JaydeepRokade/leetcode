afc390a882177fedf1619eecb36f4c7aa65fa1f4
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-02 23:48:33 +0530

    Update leet2025.cpp

--- leet2025.cpp
+++ leet2025.cpp
@@ -9,7 +9,7 @@ public:
             if (s[curr] == '.') {
                 continue;
             }
-            // jaydeep rokade jadjkv jlldll ldfl lal lallvlk lkllk akczp sdp ef  lcl lnsdlkll kakz  dnnk peefi jgj rrwp dfooegj x;wfgjp vs
+            // jaydeep rokade jadjkv jlldll ldfl lal lallvlk lkllk akczp sdp ef  lcl lnsdlkll kakz  dnnk peefi jgj rrwp dfooegj x;wfg
             int span = curr - prev - 1;
             if (prev > 0) 
                 res += s[prev];
