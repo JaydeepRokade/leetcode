e1127f58219934e1cbbc9588b1097a9f77d0bbac
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-03 23:59:45 +0530

    Update leet52.cpp

--- leet52.cpp
+++ leet52.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    int countSubarrays(vector<int>& nums, int count = 0) {
-        for (int i = 0 ; i + 2 < nums.size(); i++)
-            if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) 
-                
-                count++;
-        return count;
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int res = INT_MAX;
+        for (int val = 1; val <= 6; ++val) {
+            int top_swaps = 0, bottom_swaps = 0;
+            bool valid = true;
+            for (int i = 0; i < tops.size(); ++i) {
+                if (tops[i] != val && bottoms[i] != val) {
+                    valid = false;
+                    break;
+                }
+                if (tops[i] != val) top_swaps++;
+                if (bottoms[i] != val) bottom_swaps++;
+            }
+            if (valid)
+                res = min(res, min(top_swaps, bottom_swaps));
+        }
+        return res == INT_MAX ? -1 : res;
     }
 };
