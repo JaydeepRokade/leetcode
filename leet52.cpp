b6e8a0433be5c52885ec4798bdd3b64007dbb607
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-04 00:01:06 +0530

    Update leet52.cpp

--- leet52.cpp
+++ leet52.cpp
@@ -10,7 +10,7 @@ public:
                     valid = false;
                     break;
                 }
-                //jkahiu ksa kaskjj skk skhdvk kdck kkdv;kkh; hd;vak kvh ;ihdv;ho hf;oh hv;
+                //jkahiu ksa kaskjj skk skhdvk kdck kkdv;kkh; hd;vak kvh ;ihdv;ho 
                 if (tops[i] != val) top_swaps++;
                 if (bottoms[i] != val) bottom_swaps++;
                 
