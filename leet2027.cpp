262855dabca33c39bf046b9657fe53076cf58fd3
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-04 23:12:18 +0530

    Update leet2027.cpp

--- leet2027.cpp
+++ leet2027.cpp
@@ -5,7 +5,7 @@ class Solution {
                 return p.first * 31 + p.second;
             }
         };
-    //eskhkafh dj dlls llsli kjrgkj kjrgkj kj ;kuhdgwk kgkgk kfiuuc uuadu  udwfgij jdsfl
+    //eskhkafh dj dlls llsli kjrgkj kjrgkj kj ;kuhdgwk kgkgk kfiuuc uuadu  udwfgij j
         int numEquivDominoPairs(vector<vector<int>>& dominoes) {
             unordered_map<pair<int,int>, int, hash_pair> mpp;
     
