ea19952c9860d1515c5484997e4cccddd4e7f40f
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-06 00:00:31 +0530

    Update leet2028.cpp

--- leet2028.cpp
+++ leet2028.cpp
@@ -14,7 +14,7 @@ private:
         }
         return C;
     }
-    //dakek ,z,dlHovlSlh csa
+    //dakek ,z,dlHov
     vector<vector<long long>> power(vector<vector<long long>> base, long long exp) {
         vector<vector<long long>> result = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
         while (exp > 0) {
