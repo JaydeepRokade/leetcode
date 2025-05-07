3c4a358c2d90d71e9711f64eb53190d7acbcf912
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-08 00:33:47 +0530

    Create leet2029.cpp

---
+++ leet2029.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findMissing(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int d = arr[1] - arr[0];
+        
+        for(int i=0; i<n-1; i++){
+            
+            int diff = arr[i+1] - arr[i];
+            if(d != diff){
+                return arr[i+1] - d;
+            }
+        }
+        return arr[n-1] + d;
+    }
+};
+
