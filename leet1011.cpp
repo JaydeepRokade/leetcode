bba56adbe98b8c9f8ac5aa23a24a2a158283993e
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-11 23:56:13 +0530

    Update leet1011.cpp

--- leet1011.cpp
+++ leet1011.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-// THE HUNDRED SERIES IS STARTED FOR ONLY TH
+// THE HUNDRED SERIES IS STAR
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
         vector<int> freq(n + 1, 0);
