ccc5a4a4aa278adb15bb219e19008d7f1ee846e6
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-18 19:31:31 +0530

    Update leet2041.cpp

--- leet2041.cpp
+++ leet2041.cpp
@@ -15,7 +15,7 @@ public:
             if (valid) good.push_back(i);
         }
         for (int i : good) dp[1][i] = 1;
-        //jaydivjozioj oislkjvl lakk, klndvdkb oopaefio hnv
+        //jaydivjozioj oislkjvl lakk, klndvdkb oopae
         for (int i : good) {
             for (int j : good) {
                 rowValid[i][j] = 1;
