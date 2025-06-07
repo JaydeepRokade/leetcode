0e5720087a0bc032b197b2bb19c222ff689c2a87
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-06-07 23:51:29 +0530

    Update leet20253.cpp

--- leet20253.cpp
+++ leet20253.cpp
@@ -4,8 +4,7 @@ public:
         int n = s.size();
         priority_queue<char, vector<char>, greater<char>> pq;  // Min-heap
         unordered_map<char, vector<int>> m;  // Stores indices of each character
-        vector<bool> v(n, true);  // Marks whether each char in s should be kept
-
+        vector<bool> v(n, true);  // Marks whether 
         for (int i = 0; i < n; i++) {
             if (s[i] == '*') {
                 char temp = pq.top();  // Get smalle
