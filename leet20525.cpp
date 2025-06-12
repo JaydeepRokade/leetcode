63e174e3f93dac924936a88122ce7a049d07032e
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-06-12 23:51:38 +0530

    Update leet20525.cpp

--- leet20525.cpp
+++ leet20525.cpp
@@ -1,5 +1,6 @@
 class Solution {
-public:
+public: 
+    //vfd fd ff zxs 
     int maxAdjacentDistance(vector<int>& nums) {
         int maxi = 0;
         for (int i = 0 ; i < nums.size() - 1 ; i++)
