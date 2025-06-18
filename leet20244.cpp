55360e3715b997d092666ca3c7fd6d5a4a4b05d8
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-18 23:44:17 +0530

    Update leet20244.cpp

--- leet20244.cpp
+++ leet20244.cpp
@@ -9,7 +9,6 @@ public:
 
         for(int i=0;i<n;i+=3){
             //  nums[i]     nums[i+1]     nums[i+2]
-            //   low   
             if(nums[i+2]-nums[i]>k){
                 return {};
             }
