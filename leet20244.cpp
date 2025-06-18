d94f54dee4497351cb525101073bee1d1e2585f5
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-18 23:45:41 +0530

    Update leet20244.cpp

--- leet20244.cpp
+++ leet20244.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums.size();
         sort(begin(nums), end(nums));
         vector<vector<int>> ans;
-
         for(int i=0;i<n;i+=3){
             if(nums[i+2]-nums[i]>k){
                 return {};
@@ -13,7 +12,6 @@ public:
                 ans.push_back({nums[i], nums[i+1], nums[i+2]});
             }
         }
-
         return ans;
     }
 };
