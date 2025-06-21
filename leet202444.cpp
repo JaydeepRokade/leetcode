5277453961474f083bc720dceaac1145806ebabb
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-22 00:01:34 +0530

    Update leet202444.cpp

--- leet202444.cpp
+++ leet202444.cpp
@@ -4,18 +4,14 @@ public:
         array<int, 26> freq{};
         for (unsigned char c : word)
             ++freq[c - 'a'];
-
         vector<int> freqList;
         freqList.reserve(26);
         for (int f : freq)
             if (f > 0)
                 freqList.push_back(f);
-
         sort(begin(freqList), end(freqList));
-
         int n = freqList.size(), total = word.size(), removedLeft = 0,
             inWindowSum = 0, best = INT_MAX, r = 0;
-
         for (int l = 0; l < n; ++l) {
             int base = freqList[l];
             int limit = base + k;
