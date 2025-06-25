2ff5ef3eaf5e55b85a427a117f05c74e52fa6e16
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-25 23:30:35 +0530

    Update leet202111.cpp

--- leet202111.cpp
+++ leet202111.cpp
@@ -16,7 +16,7 @@ class Solution {
     if (k > negCount) {
       k -= negCount;   
     } else {
-      k = negCount - k + 1;  // Find (negCount - k + 1)-th abs
+      k = negCount - k + 1;  // Find (negCount - 
       sign = -1;
       swap(B1, B2);
     }
