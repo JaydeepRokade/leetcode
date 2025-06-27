ac4526b79d07261cef77ce2819aba5db623aa8d7
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-27 23:59:43 +0530

    Update leet10101.cpp

--- leet10101.cpp
+++ leet10101.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-// ah ah ah ah ah ah j
+// ah ah ah
     vector<string> divideString(string &s, int k, char fill) {
         auto [q, r]=div(s.size(), k);
         vector<string> ans(q+(r>0)); 
