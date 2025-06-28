a2acdb883bcf07f0403586ff27b94c6325a7444c
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-28 23:59:41 +0530

    Update leet1011.cpp

--- leet1011.cpp
+++ leet1011.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//Ajay quality AJ aja J
+//Ajay quality AJ 
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
         vector<int> freq(n + 1, 0);
