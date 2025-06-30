db67f73c4d755fe9f4a27c4dea5c42f1c387b6d5
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-06-30 23:56:26 +0530

    Update leet2020.cpp

--- leet2020.cpp
+++ leet2020.cpp
@@ -3,7 +3,7 @@ public:
     int findLHS(vector<int>& nums) {
         unordered_map<int, int> frequencyMap;
 
-        // Step 1: Count frequencie
+        // Step 1:
         for (int num : nums) {
             frequencyMap[num]++;
         }
