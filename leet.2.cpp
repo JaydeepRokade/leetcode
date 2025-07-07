249664cd95c6cfc72cb4b03e37d719c388f3fe64
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-07-07 23:53:49 +0530

    Update leet.2.cpp

--- leet.2.cpp
+++ leet.2.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//quality quality qkk kak kwks
+//quality quality qkk k
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int midx = m - 1;
         int nidx = n - 1;
