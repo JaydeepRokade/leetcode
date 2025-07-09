3d9f4e850ebc7973798fb39bd323a078fd754830
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-07-09 23:47:15 +0530

    Update leet202234.cpp

--- leet202234.cpp
+++ leet202234.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//Anja jqjwjvwjwj  woke seen sw sk idk kek jekk
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
         int count = startTime.size();
         vector<int> prefixSum(count + 1, 0);
