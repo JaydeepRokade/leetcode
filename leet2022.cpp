f81030730a6f64c6d1bb96eb5b82507851e9ae75
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-07-22 23:57:09 +0530

    Update leet2022.cpp

--- leet2022.cpp
+++ leet2022.cpp
@@ -1,6 +1,6 @@
 class Solution {
     public:
-//ask aj aj a kak a k
+//ask aj aj a 
         long long countSubarrays(vector<int>& nums, int minK, int maxK) {
             long long count = 0, left = 0;
             deque<int> dq_min, dq_max;
