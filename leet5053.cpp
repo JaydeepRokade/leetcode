19544263da3248ff6f154b933bebb49016f17d57
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-07-31 23:26:58 +0530

    Update leet5053.cpp

--- leet5053.cpp
+++ leet5053.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//also qjakvakqk kakvksksk 
+//also qjakvakq
     int subarrayBitwiseORs(vector<int>& arr) {
         unordered_set<int> res, cur, next;
         for (int num : arr) {
